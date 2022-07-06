7adac038069df322ac375e4e73f1550c2bec1a75
Erevney <75116258+Erevney@users.noreply.github.com>
2022-07-05 19:43:41 -0500

    feat:edit ids

--- src/routes/id.jsx
+++ src/routes/id.jsx
@@ -1,13 +1,13 @@
 import { getid } from "../data";
 import{Link, Outlet} from "react-router-dom";
-
 export default function id(){
+
 let id = getid()
 return(
 <div className="pokemon-card">
 <h2>
 {id.map((ids)=>( <Link style={{display: "block", margin:"1rem 0"}} to = {`/id/${ids.number}`} key= {ids.number}>
-Datos del Alumno:
+Datos del Alumno: Carlos Isaac Macias Herrera 
 </Link>
 ))}
 </h2>
--- src/routes/ids.jsx
+++ src/routes/ids.jsx
@@ -1,14 +1,15 @@
 import {useParams} from "react-router-dom";
 import { getIds } from "../data";
+import id from "./id";
 export default function Ids(){
 
     let params = useParams();
     let ids = getIds(parseInt(params.idsId));
     return(
         <h2>
-        <h2>id:{ids.number}</h2>
+        <h2>Matricula:190967</h2>
         <p>
-            Nombre:{ids.name}
+            Nombre:Carlos Isaac Macias Herrera
         </p>
     </h2>
     );

916e26b03a757629ab930bd9d94c16ff94c7c197
Joan Montoya <joan15montoya@gmail.com>
2023-03-13 17:23:57 -0600

    firt

---
+++ EstadosActivityTest_IDGS_9A_ILF/app/src/main/java/com/example/estadosactivitytest_idgs_9a_ilf/Home2.java
@@ -0,0 +1,14 @@
+package com.example.estadosactivitytest_idgs_9a_ilf;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Home2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home2);
+    }
+}
